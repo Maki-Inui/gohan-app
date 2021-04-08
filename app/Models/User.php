dd29bf16f1046a23b1e874a0deb92af44601f1b4
Maki-Inui <mqkichim@yahoo.co.jp>
2021-04-08 17:16:35 +0900

    login_modified

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
+/* use Laravel\Fortify\TwoFactorAuthenticatable; */
 use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -16,7 +16,7 @@ class User extends Authenticatable
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
-    use TwoFactorAuthenticatable;
+/*     use TwoFactorAuthenticatable; */
 
     /**
      * The attributes that are mass assignable.
