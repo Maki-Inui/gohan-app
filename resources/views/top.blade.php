4dff72abb297cb2529be97876ea06faa8823eb99
Maki-Inui <mqkichim@yahoo.co.jp>
2021-04-10 20:23:11 +0900

    top-page202104101649

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -6,6 +6,15 @@
 
 @auth
   <a href="{{ url('/') }}">mypage</a>
+  <form method="POST" action="{{ route('logout') }}">
+    @csrf
+
+    <x-jet-responsive-nav-link href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                    this.closest('form').submit();">
+        {{ __('Log Out') }}
+    </x-jet-responsive-nav-link>
+  </form>
 @else
   <a href="{{ route('login') }}">Login</a>
   @if (Route::has('register'))
