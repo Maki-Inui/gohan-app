4791bd80c6a53657b8e3773329a48781bda4746d
Maki-Inui <mqkichim@yahoo.co.jp>
2021-04-21 15:05:17 +0900

    rev2 modified migration files(create_shops and create_reviews)

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\Shops;
+use App\Models\Shop;
 
 
 class Review extends Model
