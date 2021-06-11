ddcf9d9b0bd02727469e9807e7d4062965d8073a
Maki-Inui <mqkichim@yahoo.co.jp>
2021-06-11 15:31:53 +0900

    modified index.blade of like_visit_history

--- resources/views/history/index.blade.php
+++ resources/views/history/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <main>
   <h3 class="text-center">{{ Auth::user()->name }}さんのお店の閲覧履歴</h3>
-  <div class="article w-4/5 lg:w-2/5">
+  <div class="">
   @if($histories->isEmpty())
-    <p>履歴はありません</p> 
+    <p class="article w-4/5 lg:w-2/5 rounded-lg">履歴はありません</p> 
   @else
     @foreach($histories as $history)
-    <ul class="article">
+    <ul class="article w-4/5 lg:w-2/5">
       <li class="text-2xl font-bold text-indigo-600"><a href="{{  route('shops.show', ['shop' => $history->shop_id]) }}">{{ $history->shop->name }}</a></li>
       <li>{{ $history->shop->area->area_name }}</li>
     </ul>
--- resources/views/visit/index.blade.php
+++ resources/views/visit/index.blade.php
@@ -3,12 +3,12 @@
 @section('content')
 <main>
   <h3 class="text-center">{{ Auth::user()->name }}さんの行ったお店一覧</h3>
-  <div class="article w-4/5 lg:w-2/5">
+  <div class="">
   @if($visits->isEmpty())
-    <p>行ったお店はありません</p>
+    <p class="article w-4/5 lg:w-2/5 rounded-lg">行ったお店はありません</p>
   @else
     @foreach($visits as $visit)
-    <ul class="article">
+    <ul class="article w-4/5 lg:w-2/5">
       <li><a href="{{  route('shops.show', $visit->shop_id) }}">{{ $visit->shop->name }}</a></li>
       <li>{{ $visit->shop->area->area_name }}</li>
     </ul>
