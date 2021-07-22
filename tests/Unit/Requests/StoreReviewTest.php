c70f9286f4e2fcb4566d30b40942ee17cf1b15f3
Maki-Inui <mqkichim@yahoo.co.jp>
2021-07-22 18:43:35 +0900

    removed RefreshDatabase from StoreReview

--- tests/Unit/Requests/StoreReviewTest.php
+++ tests/Unit/Requests/StoreReviewTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Unit\Requests;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Validator;
